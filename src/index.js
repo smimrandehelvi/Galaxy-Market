a37a4677df91e0316229eef90e5649a8a06432a0
itssaptarshi <saptarshimaitra91@gmail.com>
2022-05-11 11:47:29 +0530

    init

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from './ScrollToTop';
+import { MoralisProvider } from "react-moralis";
+
 
 ReactDOM.render(
   <BrowserRouter >
     <ScrollToTop />
+    <MoralisProvider appId="cg083RyJzWvVbLtyQ7WxQpfAyIICON4AN8MGSxqe" serverUrl="https://btbpgajnttxt.usemoralis.com:2053/server">
     <App />
+    </MoralisProvider>,
   </BrowserRouter>,
   document.getElementById('root')
 );
